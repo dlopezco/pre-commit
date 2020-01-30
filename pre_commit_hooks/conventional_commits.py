a27f6f4daef0c23537e4109436b877532e1b8f4e
Diego Lopez <dlopez@comparaonline.com>
2020-01-30 18:07:28 -0300

    add some prints to debug

--- pre_commit_hooks/conventional_commits.py
+++ pre_commit_hooks/conventional_commits.py
@@ -6,7 +6,9 @@ def main():
     # feat(apikey): added the ability to add api key to configuration
     pattern = r'(build|ci|docs|feat|fix|perf|refactor|style|test|chore|revert)(\([\w\-]+\))?:\s.*'
     filename = sys.argv[1]
+    print(filename)
     ss = open(filename, 'r').read()
+    print(ss)
     m = re.match(pattern, ss)
     if m == None: raise Exception("conventional commit validation failed")
 
