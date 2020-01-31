96a6bd2fcae184d26a6e24d3a1e4a5072ca52127
Diego Lopez <dlopez@comparaonline.com>
2020-01-31 14:27:43 -0300

    v13

--- pre_commit_hooks/conventional_commits.py
+++ pre_commit_hooks/conventional_commits.py
@@ -5,20 +5,15 @@ import subprocess
 def main():
     # example:
     # feat(apikey): added the ability to add api key to configuration
+
     pattern = r'(build|ci|docs|feat|fix|perf|refactor|style|test|chore|revert)(\([\w\-]+\))?:\s.*'
-    old, new, branch = sys.stdin.read().split()
-    print(old)
-    print(new)
-    print(branch)
-    proc = subprocess.Popen(["git", "rev-list", "--oneline","--first-parent" , "%s..%s" %(old, new)], stdout=subprocess.PIPE)
-    commitMessage=str(proc.stdout.readlines()[0])
+
+    #Get the commit file
+    commitMessageFile = open(sys.argv[1]) #The first argument is the file
+    commitMessage = commitMessageFile.read().strip()
     print(commitMessage)
-    try:
-        message = subprocess.check_output(['HEAD']).decode('UTF-8')
-    except:
-        raise Exception("No message joined for commit")
-    print(message)
-    m = re.match(pattern, message)
+
+    m = re.match(pattern, commitMessage)
     if m == None: raise Exception("conventional commit validation failed")
 
 if __name__ == "__main__":
