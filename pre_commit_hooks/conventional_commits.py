ab972f98316f8e57adca0cd3e514f30ef4f14780
Diego Lopez <dlopez@comparaonline.com>
2020-01-31 12:43:37 -0300

    fix conventional commits

--- pre_commit_hooks/conventional_commits.py
+++ pre_commit_hooks/conventional_commits.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python
 import re, sys, os
+import subprocess
+import humanhash
 
 def main():
     # example:
     # feat(apikey): added the ability to add api key to configuration
     pattern = r'(build|ci|docs|feat|fix|perf|refactor|style|test|chore|revert)(\([\w\-]+\))?:\s.*'
-    filename = sys.argv[1]
-    print(filename)
-    ss = open(filename, 'r').read()
-    print(ss)
-    m = re.match(pattern, ss)
+    try:
+        message = humanhash.humanize(subprocess.check_output(['HEAD']))
+    except:
+        raise Exception("No message joined for commit")
+    m = re.match(pattern, message)
     if m == None: raise Exception("conventional commit validation failed")
 
 if __name__ == "__main__":
