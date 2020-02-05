269e179c3e92fad8ddb07ab4dc6cb26016cfe5fd
Diego Lopez <dlopez@comparaonline.com>
2020-02-05 10:02:20 -0300

    v14

--- pre_commit_hooks/conventional_commits.py
+++ pre_commit_hooks/conventional_commits.py
@@ -1,19 +1,15 @@
 #!/usr/bin/env python
 import re, sys, os
-import subprocess
 
 def main():
     # example:
     # feat(apikey): added the ability to add api key to configuration
-
     pattern = r'(build|ci|docs|feat|fix|perf|refactor|style|test|chore|revert)(\([\w\-]+\))?:\s.*'
-
-    #Get the commit file
-    commitMessageFile = open(sys.argv[1]) #The first argument is the file
-    commitMessage = commitMessageFile.read().strip()
-    print(commitMessage)
-
-    m = re.match(pattern, commitMessage)
+    filename = sys.argv[1]
+    print(filename)
+    ss = open(filename, 'r').read()
+    print(ss)
+    m = re.match(pattern, ss)
     if m == None: raise Exception("conventional commit validation failed")
 
 if __name__ == "__main__":
